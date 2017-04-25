a27286f442ac30c0fd1c0ed88df40560ed41c510
Apple <apple@Apples-MacBook.local>
2017-04-25 18:05:30 +0500

    PickDetails

---
+++ Delvo/InitialVc.swift
@@ -0,0 +1,35 @@
+//
+//  InitialVc.swift
+//  Delvo
+//
+//  Created by Apple on 19/04/2017.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+import UIKit
+
+class InitialVc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
