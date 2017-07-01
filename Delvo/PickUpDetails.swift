c4fda7a6ff2a0790799a10ce5ddf9bb8750f83a1
Mehreen Karamat <mehreen.ku@gmail.com>
2017-07-02 00:40:01 +0500

    Added amount at Pickup Feature

--- Delvo/PickUpDetails.swift
+++ Delvo/PickUpDetails.swift
@@ -14,6 +14,18 @@ import RxCocoa
 class PickUpDetails: UIViewController  {
     
     @IBOutlet weak var DetailView: UIView!
+    @IBAction func PayAtPick(_ sender: Any) {
+        
+        if AmountButton.image(for: .normal) == UIImage(named:"PayUncheck"){
+           self.AmountButton.setImage(UIImage(named:"PayCheck"), for: .normal)
+           self.presentAlert()
+        }
+        else {
+            self.AmountButton.setImage(UIImage(named:"PayUncheck"), for: .normal)
+            Pick_Detail.PickUpAmount = ""
+        }
+    }
+    @IBOutlet weak var AmountButton: UIButton!
     @IBOutlet weak var PickDateLabel: UILabel!
     @IBOutlet weak var PickTimeLabel: UILabel!
     @IBOutlet weak var NameView: UIView!
@@ -27,8 +39,6 @@ class PickUpDetails: UIViewController  {
     @IBOutlet weak var PickUpTime: UITextField!
     @IBOutlet weak var PickUpDate: UITextField!
     @IBOutlet weak var PickUpDetail: UITextView!
-    @IBOutlet weak var PickUpAmount: UITextField!
-    @IBOutlet weak var AmountView: UIView!
     @IBOutlet weak var ToastView: UIView!
     @IBOutlet weak var CheckButton: UIButton!
     @IBOutlet weak var PickUpButtonView: UIView!
@@ -57,13 +67,11 @@ class PickUpDetails: UIViewController  {
         MoveToVc.visitPD=true
         PickUpDetail.delegate = self
         self.ToastView.isHidden = true
-        self.AmountView.isHidden = true
-        self.PickUpAmount.isHidden = true
         self.setDelegate()
         self.setOrigin()
         DMobj.AddGesture(controller: self)
         self.SenderContact.inputAccessoryView = obj.AddDoneButton(controller: self)
-        self.PickUpAmount.inputAccessoryView = obj.AddDoneButton(controller: self)
+     //   self.PickUpAmount.inputAccessoryView = obj.AddDoneButton(controller: self)
         self.TimePickerMethod()
         self.DatePickerMethod()
         self.setViews()
@@ -75,7 +83,7 @@ class PickUpDetails: UIViewController  {
         self.ContactView.SetCorners(radius: 5)
         self.AddressView.SetCorners(radius: 5)
         self.DetailView.SetCorners(radius: 5)
-        self.AmountView.SetCorners(radius: 5)
+     //   self.AmountView.SetCorners(radius: 5)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -88,10 +96,13 @@ class PickUpDetails: UIViewController  {
         self.PickUpDate.text = Pick_Detail.PickUpDate
         
         if Pick_Detail.PickUpDetail != ""{
+           
             self.PickUpDetail.text = Pick_Detail.PickUpDetail
         }
         if Pick_Detail.PickUpAmount != ""{
-            
+          
+            self.AmountButton.setImage(UIImage(named:"PayCheck"), for: .normal)
+  
             //            self.AmountView.isHidden = false
             //            self.PickUpAmount.isHidden = false
             //            self.CheckButton.setImage(UIImage(named:"PayCheck"), for: .normal)
@@ -222,13 +233,13 @@ class PickUpDetails: UIViewController  {
             
         }).addDisposableTo(diposeBag)
         
-        self.PickUpAmount.rx.text.asObservable().subscribe(onNext: {
-            text in
-            
-            Pick_Detail.PickUpAmount = self.PickUpAmount.text!
-            
-        }).addDisposableTo(diposeBag)
-        
+//        self.PickUpAmount.rx.text.asObservable().subscribe(onNext: {
+//            text in
+//            
+//            Pick_Detail.PickUpAmount = self.PickUpAmount.text!
+//            
+//        }).addDisposableTo(diposeBag)
+//        
         self.PickUpDetail.rx.text.asObservable().subscribe(onNext: {
             text in
             
@@ -245,7 +256,7 @@ class PickUpDetails: UIViewController  {
         self.PickUpDate.delegate = self
         self.PickUpTime.delegate = self
         self.PickUpDetail.delegate = self
-        self.PickUpAmount.delegate = self
+     //   self.PickUpAmount.delegate = self
     }
     
     func dismissKeyboard() {
@@ -341,6 +352,36 @@ class PickUpDetails: UIViewController  {
         super.viewWillDisappear(true)
         self.view.endEditing(true)
     }
+    
+    func presentAlert() {
+        let alertController = UIAlertController(title: "Ammount", message: "Enter amount that you want rider to pay at pick up on your behalf.", preferredStyle: .alert)
+        
+        let confirmAction = UIAlertAction(title: "Confirm", style: .default) { (_) in
+            if let field = alertController.textFields?[0] {
+               Pick_Detail.PickUpAmount = field.text!
+            } else {
+                self.AmountButton.setImage(UIImage(named:"PayUncheck"), for: .normal)
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (_) in
+          
+           self.AmountButton.setImage(UIImage(named:"PayUncheck"), for: .normal)
+        }
+        
+        alertController.addTextField { (textField) in
+            textField.placeholder = "eg. 1500"
+            if Pick_Detail.PickUpAmount != "" {
+                textField.text = Pick_Detail.PickUpAmount
+            }
+            textField.keyboardType = UIKeyboardType.numberPad
+        }
+        
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
 }
 
 extension PickUpDetails: UITextFieldDelegate, UITextViewDelegate{
@@ -387,10 +428,10 @@ extension PickUpDetails: UITextFieldDelegate, UITextViewDelegate{
             self.view.frame.origin.y = self.origin! - 150
         }
         
-        if textField == self.PickUpAmount {
-            
-            self.view.frame.origin.y = self.origin! - 150
-        }
+//        if textField == self.PickUpAmount {
+//            
+//            self.view.frame.origin.y = self.origin! - 150
+//        }
         
         if textField == self.PickUpDate {
             
